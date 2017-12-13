5e232905070341da686b2c5fa549410d682175e9
kasunmaduraeng <kasun@fishpond.co.nz>
2017-12-14 09:42:04 +1300

    test

--- modules/ssh/manifests/params.pp
+++ modules/ssh/manifests/params.pp
@@ -1,15 +1,15 @@
 class ssh::params {
   case $facts['osfamily']{
     'Debian':{
-      $package_name= 'openssh-server',
+      $package_name= 'openssh-server'
       $service_name= 'ssh'
     },
     'RedHat':{
-      $package_name= 'openssh-server',
+      $package_name= 'openssh-server'
       $service_name= 'sshd'
     },
     default:{
-      fail("${facts['opertatingsystems']} is not supported"),
+      fail("${facts['opertatingsystems']} is not supported")
     }
   }
 }
