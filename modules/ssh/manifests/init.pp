bc07d24fc8b514eab15eebd3dee6147e63367f43
kasunmaduraeng <kasun@fishpond.co.nz>
2017-12-13 15:11:25 +1300

    update inherites ssh class

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -1,5 +1,7 @@
-class ssh {
+class ssh(
+  $package_name= $::ssh::params::package_name,
+  $service_name= $::ssh::params::service_name,
+  ) inherites ::ssh::params {
   class {'ssh::install':} ->
   class {'ssh::service':}
-
 }
