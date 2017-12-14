ac76a8c3f2164be1f2d6fa370056129a9be5904d
kasunmaduraeng <kasun@fishpond.co.nz>
2017-12-15 09:56:27 +1300

    test

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,8 @@
 
-node default {
-
-}
+node default {}
 
 file {'/tmp/it_works.txt':                       
-  ensure  => present,                           
-  mode    => '0644',                           
-  content => "It works on ${ipaddress_ens3}!\n", 
+  ensure  => present,
+  mode    => '0644',
+  content => "It works on ${ipaddress_ens3}!\n",
 }
